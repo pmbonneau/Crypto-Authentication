f695f4c082391aaf0b368c1854a4849629d2e323
pmbonneau <pierremarc_999@hotmail.com>
2017-11-30 12:55:59 -0500

    Created method to add records for regular password authentication.

--- src/authentication/main.java
+++ src/authentication/main.java
@@ -5,7 +5,15 @@
  */
 package authentication;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Scanner;
+import jsonParser.JSONObject;
 
 /**
  *
@@ -16,36 +24,84 @@ public class main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
-    {
-        System.out.printf("1. Mot de passe" + '\n');
-        System.out.printf("2. Ms-CHAP-v2" + '\n');
-        System.out.printf("3. UAF" + '\n');
-        System.out.printf("4. Quitter" + '\n');
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException 
+    {      
+        int OptionChoiceMenuA = 0;
+        int OptionChoiceMenuB = 3;
         
-        int OptionChoice;
-        try (Scanner InputReader = new Scanner(System.in)) 
+        while (OptionChoiceMenuB == 3)
         {
+            OptionChoiceMenuB = 0;
+            System.out.printf("1. Mot de passe" + '\n');
+            System.out.printf("2. Ms-CHAP-v2" + '\n');
+            System.out.printf("3. UAF" + '\n');
+            System.out.printf("4. Quitter" + '\n');
+
+            Scanner InputReader = new Scanner(System.in);  
             System.out.println("Enter a number: ");
-            OptionChoice = InputReader.nextInt();
-        } 
-        
-        switch (OptionChoice) {
-            case 1:
-                System.out.println("1");
-                break;
-            case 2:
-                System.out.println("2");
-                break;
-            case 3:
-                System.out.println("3");
-                break;
-            case 4:
-                System.out.println("4");
-                break;
-            default:
-                break;
+            OptionChoiceMenuA = InputReader.nextInt();       
+
+            switch (OptionChoiceMenuA) 
+            {
+                case 1:
+                    OptionChoiceMenuA = 0;
+                    System.out.printf("1. Enregistrer un nouveau compte" + '\n');
+                    System.out.printf("2. Authentification" + '\n');
+                    System.out.printf("3. Menu précédent" + '\n');
+                    System.out.printf("4. Quitter" + '\n');
+                    System.out.println("Enter a number: ");
+                    OptionChoiceMenuB = InputReader.nextInt();
+                    AddRecord();
+                    OptionChoiceMenuB = 3;
+                    break;
+                case 2:
+                    OptionChoiceMenuA = 0;
+                    System.out.printf("1. Enregistrer un nouveau compte" + '\n');
+                    System.out.printf("2. Authentification" + '\n');
+                    System.out.printf("3. Menu précédent" + '\n');
+                    System.out.printf("4. Quitter" + '\n');
+                    System.out.println("Enter a number: ");
+                    OptionChoiceMenuB = InputReader.nextInt();
+                    break;
+                case 3:
+                    OptionChoiceMenuA = 0;
+                    System.out.printf("1. Enregistrer un nouveau compte" + '\n');
+                    System.out.printf("2. Authentification" + '\n');
+                    System.out.printf("3. Menu précédent" + '\n');
+                    System.out.printf("4. Quitter" + '\n');
+                    System.out.println("Enter a number: ");
+                    OptionChoiceMenuB = InputReader.nextInt();
+                    break;
+                case 4:
+                    System.out.println("4");
+                    break;
+                default:
+                    break;
+            }
         }
     }
     
+    public static void AddRecord() throws NoSuchAlgorithmException, IOException
+    {
+        Scanner InputReader = new Scanner(System.in);  
+        System.out.println("Enter User ID :");
+        String UserID = InputReader.nextLine();
+        System.out.println("Enter User Password :");
+        String UserPassword = InputReader.nextLine();
+
+        MessageDigest Digest = MessageDigest.getInstance("SHA1");
+        byte[] Hash = Digest.digest(UserPassword.getBytes(StandardCharsets.UTF_8));
+        UserPassword = Base64.getEncoder().encodeToString(Hash);
+
+        FileWriter writefile = new FileWriter("keybag.txt", true);
+        FileReader readfile = new FileReader("keybag.txt");
+
+        // Creating new JSON object to store encrypted info.
+        JSONObject obj = new JSONObject();
+        obj.put("UserID", UserID);
+        obj.put("UserPassword", UserPassword);
+        writefile.write(obj.toString());
+        writefile.write("\r\n");
+        writefile.close();   
+    }
 }
