8c45bdb28e03d4a38fca1e9b5930797406e47813
pmbonneau <pierremarc_999@hotmail.com>
2017-12-03 01:42:46 -0500

    Completed code for challenge response authentication.

---
+++ src/authentication/communication.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package authentication;
+
+/**
+ *
+ * @author Pierre-Marc
+ */
+public class communication 
+{
+    String Type;
+    int TransactionID = 0;
+    String Source;
+    String Destination;
+    
+    String SessionID = "";
+    String SessionCookie = "";
+    String UserID = "";
+    String Password = "";
+    String Code = "";
+    String TransactionCommand = "";
+    String ServerNonce = "";
+    String ClientNonce = "";
+    public communication(String pType, String pSource, String pDestination)
+    {
+        Type = pType;
+        Source = pSource;
+        Destination = pDestination;       
+    }
+    
+    public void setCode(String pCode)
+    {
+        Code = pCode;
+    }
+    
+    public void setSessionID(String pSessionID)
+    {
+        SessionID = pSessionID;
+    }
+    
+    public void setSessionCookie(String pSessionCookie)
+    {
+        SessionCookie = pSessionCookie;
+    }
+    
+    public void setServerNonce(String pServerNonce)
+    {
+        ServerNonce = pServerNonce;
+    }
+    
+    public void setClientNonce(String pClientNonce)
+    {
+        ClientNonce = pClientNonce;
+    }
+    
+    public void setTransactionCommand(String pTransactionCommand)
+    {
+        TransactionCommand = pTransactionCommand;
+    }
+    
+    public String getQuery()
+    {
+        TransactionID++;
+        return Type + TransactionID + ".    " + Source + "  --->  " + Destination + "   :   " + SessionID + " " + UserID + " " + Password + " " + SessionCookie + " " + TransactionCommand + " " + ClientNonce + '\n';
+    }
+    
+    public String getAnswer()
+    {
+        TransactionID++;
+        return Type + TransactionID + ".    " + Destination + "  --->  " + Source + "   :   " + SessionID + " " + Password + " " + Code + " " + SessionCookie + " " + ServerNonce + '\n';
+    }
+}
--- src/authentication/main.java
+++ src/authentication/main.java
@@ -16,6 +16,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 import javax.sound.sampled.Line;
 import jsonParser.JSONObject;
@@ -29,12 +30,12 @@ public class main {
     /**
      * @param args the command line arguments
      */
+    public static String SessionID;
+    
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException 
     {      
         int OptionChoiceMenuA = 0;
         int OptionChoiceMenuB = 3;
-        int TransactionID = 0;
-        int SessionID = 100;
         
         while (OptionChoiceMenuB == 3)
         {
@@ -59,18 +60,70 @@ public class main {
                     System.out.println("Enter a number: ");
                     OptionChoiceMenuB = InputReader.nextInt();
                     if (OptionChoiceMenuB == 1)
-                    {
-                        transaction Result = AddRecord(TransactionID);
+                    { 
+                        InputReader.reset();
+                        System.out.println("Enter User ID :");
+                        String UserID = InputReader.next();
+                        System.out.println("Enter User Password :");
+                        String UserPassword = InputReader.next();
+
+                        communication CommAddNewRecord = new communication("E", "C", "S");
+                        CommAddNewRecord.UserID = UserID;
+                        CommAddNewRecord.Password = UserPassword;
+                        System.out.printf(CommAddNewRecord.getQuery());
+                        communication Result = AddRecordPassword(CommAddNewRecord, UserID, UserPassword);
                         System.out.printf(Result.getAnswer());
-                        TransactionID = TransactionID + 2;
                     }
                     else
                     if (OptionChoiceMenuB == 2)
-                    {
-                        transaction Result = Authenticate(TransactionID, Integer.toString(SessionID));
+                    { 
+                        InputReader.reset();
+                        System.out.println("Enter User ID :");
+                        String UserID = InputReader.next();
+                        System.out.println("Enter User Password :");
+                        String UserPassword = InputReader.next();
+
+                        communication CommAuthenticate = new communication("A", "C", "S");
+                        CommAuthenticate.UserID = UserID;
+                        CommAuthenticate.Password = UserPassword;
+                        CommAuthenticate.SessionID = generateFiveDigitsRandomNumber();
+                        SessionID = CommAuthenticate.SessionID;
+                        System.out.printf(CommAuthenticate.getQuery());
+                        communication Result = AuthenticatePassword(CommAuthenticate, UserID, UserPassword);
                         System.out.printf(Result.getAnswer());
-                        TransactionID = TransactionID + 2;
-                        SessionID++;
+                        
+                        boolean EndTransactions = false;
+                        communication CommTransaction = new communication("T", "C", "S");
+                        while (EndTransactions == false)
+                        {
+                            InputReader.reset();
+                            System.out.println("Enter transaction command :");
+                            String TransactionCommand = InputReader.next();
+                            
+                            if (TransactionCommand.equals("end"))
+                            {
+                                System.out.println("End transaction");
+                                break;
+                            }
+                            
+                            System.out.println("Enter session cookie :");
+                            String SessionCookie = InputReader.next();
+                            CommTransaction.SessionID = SessionID;
+                            CommTransaction.TransactionCommand = TransactionCommand;
+                            CommTransaction.SessionCookie = CommAuthenticate.SessionCookie;
+                            System.out.printf(CommTransaction.getQuery());
+
+                            if (SessionCookie.equals(CommTransaction.SessionCookie))
+                            {
+                                CommTransaction.Code = Integer.toString(200);
+                            }
+                            else
+                            {
+                                CommTransaction.Code = Integer.toString(401);
+                            }
+                            System.out.printf(CommTransaction.getAnswer());
+                        }
+                        
                     }
                     OptionChoiceMenuB = 3;
                     break;
@@ -82,6 +135,59 @@ public class main {
                     System.out.printf("4. Quitter" + '\n');
                     System.out.println("Enter a number: ");
                     OptionChoiceMenuB = InputReader.nextInt();
+                    if (OptionChoiceMenuB == 1)
+                    { 
+                        InputReader.reset();
+                        System.out.println("Enter User ID :");
+                        String UserID = InputReader.next();
+                        System.out.println("Enter User Password :");
+                        String UserPassword = InputReader.next();
+
+                        communication CommAddNewRecord = new communication("E", "C", "S");
+                        CommAddNewRecord.UserID = UserID;
+                        CommAddNewRecord.Password = UserPassword;
+                        System.out.printf(CommAddNewRecord.getQuery());
+                        communication Result = AddRecordChallengeResponse(CommAddNewRecord, UserID, UserPassword);
+                        System.out.printf(Result.getAnswer());
+                    }
+                    else
+                    if (OptionChoiceMenuB == 2)
+                    {
+                        InputReader.reset();
+                        System.out.println("Enter User ID :");
+                        String UserID = InputReader.next();
+                        System.out.println("Enter User Password :");
+                        String UserPassword = InputReader.next();   
+
+                        communication CommAuthenticate = new communication("A", "C", "S");
+                        CommAuthenticate.UserID = UserID;
+                        CommAuthenticate.SessionID = generateFiveDigitsRandomNumber();
+                        SessionID = CommAuthenticate.SessionID;
+                        System.out.printf(CommAuthenticate.getQuery());
+                        
+                        String ServerNonce = generateFiveDigitsRandomNumber();
+                        CommAuthenticate.ServerNonce = ServerNonce;
+                        System.out.printf(CommAuthenticate.getAnswer());
+                          
+                        MessageDigest Digest = MessageDigest.getInstance("MD5");
+                        byte[] Hash = Digest.digest(UserPassword.getBytes(StandardCharsets.UTF_8));
+                        UserPassword = Base64.getEncoder().encodeToString(Hash);
+                        
+                        String AuthInfoClientAttendu = SessionID + ServerNonce + UserPassword;
+                        
+                        MessageDigest SecondaryDigest = MessageDigest.getInstance("SHA1");
+                        byte[] SecondaryHash = SecondaryDigest.digest(AuthInfoClientAttendu.getBytes(StandardCharsets.UTF_8));
+                        AuthInfoClientAttendu = Base64.getEncoder().encodeToString(SecondaryHash);
+                        CommAuthenticate.Password = AuthInfoClientAttendu;
+                        
+                        
+                        //String ClientNonce = generateFiveDigitsRandomNumber();
+                        //CommAuthenticate.ClientNonce = ClientNonce;
+                        //CommAuthenticate.UserID = "";
+                        communication Result = AuthenticateChallengeResponse(CommAuthenticate, UserID, UserPassword);
+                        System.out.printf(CommAuthenticate.getQuery());
+                        System.out.printf(Result.getAnswer());
+                    }
                     break;
                 case 3:
                     OptionChoiceMenuA = 0;
@@ -101,19 +207,8 @@ public class main {
         }
     }
     
-    public static transaction AddRecord(int TransactionNumber) throws NoSuchAlgorithmException, IOException
+    public static communication AddRecordPassword(communication ClientInitialCommunication, String UserID, String UserPassword) throws NoSuchAlgorithmException, IOException
     {
-        Scanner InputReader = new Scanner(System.in);  
-        System.out.println("Enter User ID :");
-        String UserID = InputReader.nextLine();
-        System.out.println("Enter User Password :");
-        String UserPassword = InputReader.nextLine();
-        
-        transaction TrAddNewRecord = new transaction("E", TransactionNumber, "C", "S");
-        TrAddNewRecord.UserID = UserID;
-        TrAddNewRecord.Password = UserPassword;
-        System.out.printf(TrAddNewRecord.getQuery());
-
         MessageDigest Digest = MessageDigest.getInstance("SHA1");
         byte[] Hash = Digest.digest(UserPassword.getBytes(StandardCharsets.UTF_8));
         UserPassword = Base64.getEncoder().encodeToString(Hash);
@@ -128,24 +223,32 @@ public class main {
         writefile.write(obj.toString());
         writefile.write("\r\n");
         writefile.close();
-        TrAddNewRecord.Code = "200";
-        return TrAddNewRecord;
+        ClientInitialCommunication.Code = "200";
+        return ClientInitialCommunication;
     }
     
-    public static transaction Authenticate(int TransactionNumber, String SessionID) throws NoSuchAlgorithmException, IOException
+        public static communication AddRecordChallengeResponse(communication ClientInitialCommunication, String UserID, String UserPassword) throws NoSuchAlgorithmException, IOException
     {
-        Scanner InputReader = new Scanner(System.in);  
-        System.out.println("Enter User ID :");
-        String UserID = InputReader.nextLine();
-        System.out.println("Enter User Password :");
-        String UserPassword = InputReader.nextLine();
-        
-        transaction TrAuthenticate = new transaction("A", TransactionNumber, "C", "S");
-        TrAuthenticate.UserID = UserID;
-        TrAuthenticate.Password = UserPassword;
-        TrAuthenticate.SessionID = SessionID;
-        System.out.printf(TrAuthenticate.getQuery());
-        
+        MessageDigest Digest = MessageDigest.getInstance("MD5");
+        byte[] Hash = Digest.digest(UserPassword.getBytes(StandardCharsets.UTF_8));
+        UserPassword = Base64.getEncoder().encodeToString(Hash);
+
+        FileWriter writefile = new FileWriter("keybag.txt", true);
+        FileReader readfile = new FileReader("keybag.txt");
+
+        // Creating new JSON object to store encrypted info.
+        JSONObject obj = new JSONObject();
+        obj.put("UserID", UserID);
+        obj.put("UserPassword", UserPassword);
+        writefile.write(obj.toString());
+        writefile.write("\r\n");
+        writefile.close();
+        ClientInitialCommunication.Code = "200";
+        return ClientInitialCommunication;
+    }
+    
+    public static communication AuthenticatePassword(communication ClientInitialCommunication, String UserID, String UserPassword) throws NoSuchAlgorithmException, IOException
+    {    
         MessageDigest Digest = MessageDigest.getInstance("SHA1");
         byte[] Hash = Digest.digest(UserPassword.getBytes(StandardCharsets.UTF_8));
         UserPassword = Base64.getEncoder().encodeToString(Hash);
@@ -158,15 +261,49 @@ public class main {
             JSONObject obj = new JSONObject(lines.get(i));
             String ReadUserID = obj.get("UserID").toString();
             String ReadUserPassword =  obj.get("UserPassword").toString();
-            if(ReadUserID == UserID && ReadUserPassword == UserPassword)
+            if(ReadUserID.equals(UserID) && ReadUserPassword.equals(UserPassword))
             {
-                TrAuthenticate.Code = "200";
-                return TrAuthenticate;
+                String SessionCookie = generateFiveDigitsRandomNumber();
+                ClientInitialCommunication.setSessionCookie(SessionCookie);
+                ClientInitialCommunication.Code = "200";
+                return ClientInitialCommunication;
             }
-        }
+        }      
+        ClientInitialCommunication.Code = "401";       
+        return ClientInitialCommunication;
+    }
+    
+    public static communication AuthenticateChallengeResponse(communication ClientInitialCommunication, String UserID, String UserPassword) throws NoSuchAlgorithmException, IOException
+    {          
+        String AuthInfoClient = SessionID + ClientInitialCommunication.ServerNonce;
         
-        TrAuthenticate.Code = "401";
+        Path Path = Paths.get("keybag.txt");
+        List<String> lines = Files.readAllLines(Path);
         
-        return TrAuthenticate;
+        for (int i = 0; i < lines.size(); i++)
+        {
+            JSONObject obj = new JSONObject(lines.get(i));
+            String ReadUserID = obj.get("UserID").toString();
+            String ReadAuthInfoClient =  obj.get("UserPassword").toString();
+            ReadAuthInfoClient = AuthInfoClient + ReadAuthInfoClient;
+            MessageDigest SecondaryDigest = MessageDigest.getInstance("SHA1");
+            byte[] SecondaryHash = SecondaryDigest.digest(ReadAuthInfoClient.getBytes(StandardCharsets.UTF_8));
+            ReadAuthInfoClient = Base64.getEncoder().encodeToString(SecondaryHash);
+            if(ReadUserID.equals(UserID) && ReadAuthInfoClient.equals(ClientInitialCommunication.Password))
+            {
+                String SessionCookie = generateFiveDigitsRandomNumber();
+                ClientInitialCommunication.setSessionCookie(SessionCookie);
+                ClientInitialCommunication.Code = "200";
+                return ClientInitialCommunication;
+            }
+        }      
+        ClientInitialCommunication.Code = "401";       
+        return ClientInitialCommunication;
+    }
+    
+    public static String generateFiveDigitsRandomNumber()
+    {
+        Random rnd = new Random();
+        return Integer.toString(10000 + rnd.nextInt(90000));
     }
 }
